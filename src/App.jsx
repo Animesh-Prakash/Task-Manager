6e8fe42d7a6be7bff59ffa0c9368864b67b90c3b
Animesh-Prakash <animeshp1607@gmail.com>
2024-10-01 16:33:17 +0530

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -69,29 +69,32 @@ function App() {
      <Navbar/>
      <div className=" container bg-violet-100 rounded-xl mx-auto p-5 my-5 min-h-[80vh] ">
           <div className="addTodo my-5">
-           <h2 className='text-lg font-bold text-center'>Add a Todo</h2>
+           <h2 className='text-lg font-bold text-center sm:text-xl'>Add a Todo</h2>
            <div className='text-center'>
            <input type="text" onChange={handleChange} value={todo} className='w-1/2 sm: w-10/12 my-3 p-1'/>
            <button onClick={handleAdd} disabled={todo.length<=3} className='bg-violet-800 hover:bg-violet-950 disabled:bg-violet-700 p-4 py-1.5 text-white rounded-md mx-6 text-sm font-bold cursor-pointer '>Add</button>
            </div>
   </div>
-          <input onChange={toggleFinished} type="checkbox" checked={showFinished} /> Show Finished
+          <input onChange={toggleFinished} type="checkbox" checked={showFinished} className='sm:text-lg' /> Show Finished
         <h1 className='font-bold text-lg'>Your todos</h1>
 
         <div className="todos">
-          {todos.length===0 && <div className='m-5'>No todos to display</div>}
+          {todos.length===0 && <div className='m-5 sm:text-lg'>No todos to display</div>}
           {todos.map(item=>{
        
           return (showFinished || !item.isCompleted) && <div key={item.id} className="todo flex w-1/4 my-3 justify-between">
-            <div className='flex gap-5'>
+            <div className='flex  w-full'>
+            <div className='flex gap-5 sm:text-lg'>
             <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} id=''/>
-            <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
+            <div className={item.isCompleted?"line-through break-words":"break-words"}>{item.todo}</div>
             </div>
        
-            <div className="buttons flex h-full">
+            <div className="buttons flex h-full mx-3">
               <button onClick={(e)=>handleEdit(e,item.id)} className='bg-violet-800 hover:bg-violet-950 p-3 py-1 text-white rounded-md mx-1 text-sm font-bold'>Edit</button>
               <button onClick={(e)=>{handleDelete(item.id)}} className='bg-violet-800 hover:bg-violet-950 p-3 py-1 text-white rounded-md mx-1 text-sm font-bold'>Delete</button>
             </div>
+            </div>
+            
           </div>
            })}
         </div>
